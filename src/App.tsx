1c341178e4cc5ac7ee033990283618a18178ef2b
jonty007 <vivek.singh2994@gmail.com>
2022-04-14 21:46:26 +0530

    pricing component

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { ReactComponent as Instagram } from "./social/insta.svg";
 import { ReactComponent as Medium } from "./social/medium.svg";
 import BasicTabs from "./tabs";
 import Features from "./Features";
+import Pricing from "./Pricing";
 
 function App() {
   return (
@@ -65,6 +66,7 @@ function App() {
         </Box>
       </Box>
       <Features />
+      <Pricing />
     </div>
   );
 }
---
+++ src/Pricing.tsx
@@ -0,0 +1,38 @@
+import Box from "@mui/material/Box";
+import "./Pricing.css";
+
+function Pricing() {
+  return (
+    <Box className="pricing-container">
+      <Box className="horizontal-grid-line-1" />
+      <Box className="horizontal-grid-line-2" />
+      <Box className="horizontal-grid-line-3" />
+      <Box className="horizontal-grid-line-4" />
+      <Box className="vertical-grid-line-1" />
+      <Box className="vertical-grid-line-2" />
+      <Box className="vertical-grid-line-3" />
+      <Box className="vertical-grid-line-4" />
+      <Box className="vertical-grid-line-5" />
+      <Box className="vertical-grid-line-6" />
+      <Box className="vertical-grid-line-7" />
+      <Box className="pricing-heading">Pricing</Box>
+      <Box className="pricing-option-1">
+        <b>Free</b> forever on Testnet
+      </Box>
+      <Box className="pricing-option-2">
+        <b>0.1%</b> of amount sent
+      </Box>
+      <Box className="pricing-option-3">
+        <b>1 cent</b> per smart object create or update
+      </Box>
+      <Box className="pricing-explainer">
+        Choose from three pricing options, switch anytime:
+      </Box>
+      <Box className="gly14" />
+      <Box className="gly15" />
+      <Box className="gly16" />
+    </Box>
+  );
+}
+
+export default Pricing;
