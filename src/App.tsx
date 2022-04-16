2591ff5714b40069ab6e36035fc14bdd6952677b
jonty007 <vivek.singh2994@gmail.com>
2022-04-16 16:31:18 +0530

    css fixes

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { ReactComponent as Telegram } from "./social/telegram.svg";
 import { ReactComponent as YouTube } from "./social/youtube.svg";
 import { ReactComponent as Discord } from "./social/discord.svg";
 import { ReactComponent as GitHub } from "./social/github.svg";
+import { ReactComponent as Twitter } from "./social/twitter.svg";
 import { ReactComponent as LinkedIn } from "./social/linkedin.svg";
 import { ReactComponent as Instagram } from "./social/insta.svg";
 import { ReactComponent as Medium } from "./social/medium.svg";
@@ -36,7 +37,7 @@ function App() {
               <a href="#examples">Examples</a>
               <a href="https://docs.bitcoincomputer.io" target="_blank">
                 Docs
-                <ArrowLink/>
+                <ArrowLink />
               </a>
               <a href="#about">About</a>
             </nav>
@@ -49,7 +50,49 @@ function App() {
       <Pricing />
       <Examples />
       <About />
-      <Footer />
+      {/* <Footer /> */}
+      <Box className='contact-container'>
+        <Box className="vertical-grid-line-1" />
+        <Box className="vertical-grid-line-2" />
+        <Box className="vertical-grid-line-3" />
+        <Box className="vertical-grid-line-4" />
+        <Box className="vertical-grid-line-5" />
+        <Box className="vertical-grid-line-6" />
+        <Box className="vertical-grid-line-7" />
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1 }} className='ContentBox ColorWhite'>
+          <Grid item xs={4} className="contact-email-wrapper">
+            <p className="contact-email-wrapper-first">Get in touch</p>
+            <p className="contact-email-wrapper-second">hello@bitcoincomputer.io</p>
+          </Grid>
+          <Grid item xs={4} className="social-links">
+            <Grid item xs={2}></Grid>
+            <Grid item xs={8}>
+              <a href="www.twitter.com" target="_blank">
+                <Twitter height="50px" width="50px" viewBox="0 0 26 26"></Twitter>
+              </a>
+              <a href="www.twitter.com" target="_blank">
+                <Telegram height="50px" width="50px" viewBox="0 0 26 26"></Telegram>
+              </a>
+              <a href="www.twitter.com" target="_blank">
+                <YouTube height="50px" width="50px" viewBox="0 0 26 26"></YouTube>
+              </a>
+              <a href="www.twitter.com" target="_blank">
+                <Discord height="50px" width="50px" viewBox="0 0 26 26"></Discord>
+              </a>
+              <a href="www.twitter.com" target="_blank">
+                <GitHub height="50px" width="50px" viewBox="0 0 26 26"></GitHub>
+              </a>
+            </Grid>
+            <Grid item xs={2}></Grid>
+          </Grid>
+          <Grid item xs={4} className="ColorWhite social-links">
+            <p>
+              Copyright © 2022 Bitcoin Computer. All rights reserved.
+            </p>
+          </Grid>
+
+        </Grid>
+      </Box>
     </div>
   );
 }
