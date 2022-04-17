e2080b42de00e52f3f9b2121be361c06f229a07c
jonty007 <vivek.singh2994@gmail.com>
2022-04-17 19:07:46 +0530

    Fix warnings in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import logo from "./logo.svg";
-import * as React from "react";
-
 import "./App.css";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import { ReactComponent as BitcoinHeader } from "./social/header.svg";
 import { ReactComponent as Telegram } from "./social/telegram.svg";
 import { ReactComponent as YouTube } from "./social/youtube.svg";
 import { ReactComponent as Discord } from "./social/discord.svg";
 import { ReactComponent as GitHub } from "./social/github.svg";
 import { ReactComponent as Twitter } from "./social/twitter.svg";
-import { ReactComponent as LinkedIn } from "./social/linkedin.svg";
-import { ReactComponent as Instagram } from "./social/insta.svg";
-import { ReactComponent as Medium } from "./social/medium.svg";
-import BasicTabs from "./tabs";
 import Features from "./Features";
 import Pricing from "./Pricing";
 import Examples from "./Examples";
 import About from "./About";
-import Footer from "./Footer";
 import Introduction from "./Introduction";
 import { ReactComponent as ArrowLink } from "./social/arrow.svg";
 
@@ -28,14 +19,18 @@ function App() {
       <header className="header">
         <Grid container>
           <Grid item xs={3} sx={{ padding: "20px", textAlign: "left" }}>
-            <img src="logo/BitcoinComputer-Logo.png" height="40px" />
+            <img src="logo/BitcoinComputer-Logo.png" height="40px" alt="logo" />
           </Grid>
           <Grid item xs={9} className="navigation">
             <nav>
               <a href="#features">Features</a>
               <a href="#pricing">Pricing</a>
               <a href="#examples">Examples</a>
-              <a href="https://docs.bitcoincomputer.io" target="_blank">
+              <a
+                href="https://docs.bitcoincomputer.io"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Docs
                 <ArrowLink />
               </a>
