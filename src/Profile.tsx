81c5c2e7db3ad7dc533cb0d8c0fac34bc5ff2a45
jonty007 <vivek.singh2994@gmail.com>
2022-04-21 18:06:40 +0530

    remaining elements scaled

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -10,14 +10,18 @@ export interface ProfileProps {
 
 function Profile(props: ProfileProps) {
   return (
-    <Box className="profile-container">
-      <img className="profile-picture" src="/portrait.png" alt="profile" />
-      <Box className="profile-name">{props.name}</Box>
-      <Box className="profile-text">{props.text}</Box>
-      <Link className="social-1" href="http://www.google.com" target="_blank" />
-      <Link className="social-2" href="http://www.google.com" target="_blank" />
-      <Link className="social-3" href="http://www.google.com" target="_blank" />
-      <Link className="social-4" href="http://www.google.com" target="_blank" />
+    <Box sx={{ height: "100%" }}>
+      <Box className="profile-container">
+        <img className="profile-picture" src="/portrait.png" alt="profile" />
+        <Box className="profile-name">{props.name}</Box>
+        <Box className="profile-text">{props.text}</Box>
+      </Box>
+      <Box className="social-links-about">
+        <Link className="social-1" href="http://www.google.com" target="_blank" />
+        <Link className="social-2" href="http://www.google.com" target="_blank" />
+        <Link className="social-3" href="http://www.google.com" target="_blank" />
+        <Link className="social-4" href="http://www.google.com" target="_blank" />
+      </Box>
     </Box>
   );
 }
